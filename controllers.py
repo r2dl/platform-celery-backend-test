9114f6709b3d1b5a66cd0e35d31a2cae70558efc
Edward Heywood-Everett <edwardheywood-everett@MBPbackX26MJHD2.lan>
2022-04-04 09:47:20 +0100

    implemented a simple health check endpoint with blueprints. Also included this branch in the gh actions yaml

---
+++ controllers.py
@@ -0,0 +1,13 @@
+import json
+from flask import Response, Blueprint
+
+
+primary_controller = Blueprint("primary_controller", __name__)
+
+@primary_controller.route("/health", methods=["GET"])
+def health_check():
+    return Response(
+        response=json.dumps({"health_check": "passed"}, indent=4),
+        status=200,
+        mimetype="application/json",
+    )
--- main_factory.py
+++ main_factory.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, Response
+from controllers import primary_controller
 
 
 def create_app():
     app = Flask(__name__)
-
+    app.register_blueprint(primary_controller)
     return app
+
