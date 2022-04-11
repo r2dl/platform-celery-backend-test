e8763f759865f9e87d5a31c67cb8e7629daa6502
Ed H-E <edheyev@gmail.com>
2022-04-11 12:24:45 +0100

    changing testing

--- main_service/test_main_service.py
+++ main_service/test_main_service.py
@@ -3,8 +3,7 @@ import json
 
 def test_service_get(client):
     response = client.get("/")
-    assert response.status == "200 OK"
-    assert response.json == {"results": "example_get"}
+    assert response.status_code == 200
 
 
 def test_service_post(client):
@@ -12,10 +11,8 @@ def test_service_post(client):
     headers = {"Content-Type": "application/json", "Accept": "application/json"}
     response = client.post("/", data=json.dumps(data), headers=headers)
     assert response.status_code == 201
-    assert response.json == {"results": "example_post"}
 
 
 def test_service_delete(client):
     response = client.delete("/")
     assert response.status_code == 202
-    assert response.json == {"results": "example_delete"}
