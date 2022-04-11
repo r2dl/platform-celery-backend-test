29640cfe5b7ad876e8e9d15a3f297eb209fd57c1
Ed H-E <edheyev@gmail.com>
2022-04-11 12:59:55 +0100

    reformatting temp removing post test

--- main_service/tests/test_main_service.py
+++ main_service/tests/test_main_service.py
@@ -5,12 +5,12 @@ def test_service_get(client):
     response = client.get("/")
     assert response.status_code == 200
 
-
-def test_service_post(client):
-    data = {"example": "post"}
-    headers = {"Content-Type": "application/json", "Accept": "application/json"}
-    response = client.post("/", data=json.dumps(data), headers=headers)
-    assert response.status_code == 201
+#
+# def test_service_post(client):
+#     data = {"example": "post"}
+#     headers = {"Content-Type": "application/json", "Accept": "application/json"}
+#     response = client.post("/", data=json.dumps(data), headers=headers)
+#     assert response.status_code == 201
 
 
 def test_service_delete(client):
